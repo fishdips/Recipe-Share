dc4d0fbdac3d9e5660bd66dd50c5459f0531c29c
LAB23_USER ON WS30 <L23X16W30@LabHCI.net>
2025-09-24 16:41:20 +0800

    Login page

--- recipe_share/recipe/urls.py
+++ recipe_share/recipe/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
+    path('login/', views.login_page, name='login_page'),
 ]
--- recipe_share/recipe/views.py
+++ recipe_share/recipe/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+def login_page(request):
+    return render(request, 'login_page.html')
