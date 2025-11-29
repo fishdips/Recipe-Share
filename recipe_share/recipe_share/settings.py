a4769dc441144bd2276aee2f9dd36448c176d855
Jay-Z <jay-z.olimba@cit.edu>
2025-11-29 22:51:44 +0800

    fixed syntax

--- recipe_share/recipe_share/settings.py
+++ recipe_share/recipe_share/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3&_a$cbcexg#i-h4a83vakkx0lr&x+e3oofk*lkt%4r^9e(@)w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [https://recipe-share-4f7x.onrender.com]
+ALLOWED_HOSTS = ["https://recipe-share-4f7x.onrender.com"]
 
 
 # Application definition
